aaf5a7a1a538aa4b0b911e180c98143c10cbd303
杉野正樹 <himazin0606@gmail.com>
2023-06-11 08:47:58 +0900

    本番環境でのコントローラーの振る舞いの確認

--- app/controllers/span_controller.rb
+++ app/controllers/span_controller.rb
@@ -15,7 +15,7 @@ class SpanController < ApplicationController
     offsuit_sum = selection(high_offsuit,low_offsuit)
     span_data = []
     span_data = span_data.concat(pair(@span[:high_pair].to_s,@span[:low_pair].to_s),suit(num_sum),offsuit(offsuit_sum))
-    @span[:span] = "c"
+    @span[:span] = ["2"]
     @span.save
     redirect_to user_path(current_user)
   end
